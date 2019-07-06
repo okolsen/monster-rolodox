4d3f77145835c787b74a7bb27b65a4b0c1824fff
Ole Kristian Olsen <okolsen123@gmail.com>
2019-07-07 00:23:26 +0200

    Updates

---
+++ precache-manifest.0ba8ef2d75beee8723d9cf47eb9b912a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fb53871ba5af25ed7a7aaf678108e6d",
+    "url": "/monster-rolodox/index.html"
+  },
+  {
+    "revision": "1f1449e89deba178b605",
+    "url": "/monster-rolodox/static/css/main.a91b2b92.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monster-rolodox/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "1f1449e89deba178b605",
+    "url": "/monster-rolodox/static/js/main.1f2d171c.chunk.js"
+  },
+  {
+    "revision": "4a977b54e5084b43fe8a",
+    "url": "/monster-rolodox/static/js/runtime~main.8307d2ec.js"
+  }
+]);
